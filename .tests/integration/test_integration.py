fa09e3ddb204d8009d99a5947570e513d66f919d
rbpatt2019 <rb.patterson.cross@gmail.com>
2021-07-14 17:09:28 +0100

    test(integration): adds integration tests
    Using the structure from the unit tests, this provides a functioning minimal integration tests.

---
+++ .tests/integration/test_integration.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+import os
+import shutil
+import subprocess as sp
+import sys
+from pathlib import Path, PurePosixPath
+from tempfile import TemporaryDirectory
+
+sys.path.insert(0, os.path.dirname(__file__))
+
+import common
+
+
+def test_process():
+
+    with TemporaryDirectory() as tmpdir:
+        workdir = Path(tmpdir) / "workdir"
+        data_path = PurePosixPath(".tests/integration/data")
+        expected_path = PurePosixPath(".tests/integration/expected")
+        config_path = PurePosixPath(".tests/integration/config")
+
+        # Copy data to the temporary workdir.
+        shutil.copytree(data_path, workdir)
+        shutil.copytree(config_path, workdir / "config")
+
+        # dbg
+        print("results/process/ASCL1_isoforms.csv", file=sys.stderr)
+
+        # Run the test job.
+        sp.check_output(
+            [
+                "python",
+                "-m",
+                "snakemake",
+                "results/process/ASCL1_isoforms.csv",
+                "-j1",
+                "--keep-target-files",
+                "--use-conda",
+                "--conda-frontend",
+                "mamba",
+                "--directory",
+                workdir,
+            ]
+        )
+
+        # Check the output byte by byte using cmp.
+        # To modify this behavior, you can inherit from common.OutputChecker in here
+        # and overwrite the method `compare_files(generated_file, expected_file),
+        # also see common.py.
+        common.OutputChecker(data_path, expected_path, workdir).check()
